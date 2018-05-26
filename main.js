3188f5a16c115fb3817ae9196fac0f3ce5ef0d2f
alveeno <nguyenalvind@gmail.com>
2018-05-26 01:16:32 -0700

    fixed static port set to 3000, should change to heroku's assigned port now.

--- main.js
+++ main.js
@@ -20,4 +20,6 @@ app.get('/*', function (req, res) {
     //res.sendFile(__dirname + "/index.html");
   });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
